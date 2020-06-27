5005a9901af439cf50e12bf4ca40a2c2c8cf24fa
System Administrator <root@Lims-iMac.realtek>
2020-06-27 21:54:39 +0800

    Updates

---
+++ precache-manifest.574ccc09cfa926c95f187d4a97a6664d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb330d61ac2d69c3aa5b01c9f75b7db0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4750e6e560a29f2b4142",
+    "url": "/monsters-rolodex/static/css/main.b42a9d1e.chunk.css"
+  },
+  {
+    "revision": "5ceb71a0b3552c2613c8",
+    "url": "/monsters-rolodex/static/js/2.95da3331.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.95da3331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4750e6e560a29f2b4142",
+    "url": "/monsters-rolodex/static/js/main.f5a79ea2.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
